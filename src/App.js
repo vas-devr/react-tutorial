aca1f603157d1660f624a0501b558cf3ea527063
derrek1778 <derrek1789@gmail.com>
2020-03-08 16:14:32 -0400

    module 4 changes

--- src/App.js
+++ src/App.js
@@ -2,60 +2,99 @@ import React, {Component} from 'react';
 //import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import './Person/Person.css';
 
 class App extends Component{
 //  const App = props => {
  
   state =  ({
     persons: [
-      { name: 'max', age: 28 },
-      { name: 'saari', age: 26 }
+      { id: 'qq',name: 'max', age: 28 },
+      { id: 'we',name: 'saari', age: 26 }
     ], 
-    
+    buttonText:'Show Persons'
   });
      
 
-  switchNameHandler = (newName) => {
-    //console.log('Was Clicked');   
-    //this.state.persons[0].name = 'maxi0';
-    this.setState ({
-      persons: [
-        { name: 'Max', age: 30 },
-        { name: newName, age:30 }
-      ], 
-      otherState: 'personsState.otherState'
-    });
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.splice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: event.target.value, age:29 },
-        { name: event.target.value, age:29}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => { //returns the index if the id of the user matches
+      return p.id === id;
+    });
+
+
+    const person = {
+      ...this.state.persons[personIndex] //spread the existing state and gets that particular person object rather than pointing to it 
+    };
+
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+
 
   render() {
+
+    const bgStyle = {
+      backgroundColor: 'green',
+      color:'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      boxShadow: '0px 2px 3px  #ccc',
+      borderRadius: '5px',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+    
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              name={person.name}
+              age={person.age}
+              click={() => this.deletePersonHandler(index)}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event,person.id)}/>
+          })}
+       </div>
+      );
+
+      bgStyle.backgroundColor = 'red'
+    };
+
+    
   return (
     <div className="App">
       <h1>Hi There</h1>
-      <h1>test aother </h1>
-      <div>tester</div>
-      <button onClick={() => this.switchNameHandler('Vashu')}>Switch Name</button>
-      <Person
-        name={this.state.persons[0].name}
-        age={this.state.persons[0].age} click={() => this.switchNameHandler('Vashudeb')}
-        changed={this.nameChangedHandler}
-      > Vasudev
-        </Person>
-        <Person
-        name={this.state.persons[1].name}
-        age={this.state.persons[1].age} click={() => this.switchNameHandler('Vashud')}
-        changed={this.nameChangedHandler}
-      > Vasudev
-        </Person>
-    </div>   
+      <h1>test another </h1>
+      <button
+        style={bgStyle}
+        onClick={this.togglePersonHandler}>{this.state.buttonText}</button>
+      
+      {persons}
+      
+    </div>  
   );
 
   //return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi, I\'am react app'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,9 @@ import React from 'react';
 const person = (props) => {
 
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}> I am a {props.name} and {props.age} and  {Math.floor(Math.random() * 30)} person</p>
-            <p> {props.children}</p>
+           {/*  <p> {props.children}</p> */}
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     );
